369118750e41940085095a20b15866222389ae0d
Jenovai <andrea.jenovai@gmail.com>
2021-03-22 13:49:28 +0100

    7. feladat

--- fogyasztas.js
+++ fogyasztas.js
@@ -1,7 +1,11 @@
 let km = prompt("Hány kilométert mentél?")
 let szam = new RegExp('\\d')
-
 while(szam.test(km) == false || km < 1000){
     km = prompt("Hány kilométert mentél?")
+}
 
+
+let tankolas = prompt("Hány litert tankoltás?")
+while (tankolas > 1 && tankolas < 50){
+    tankolas = prompt("Hány litert tankoltás?")
 }
