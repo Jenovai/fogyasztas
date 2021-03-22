bb9a9938b3ec01bf3b7e43283a9d94729f32f1c1
Jenovai <andrea.jenovai@gmail.com>
2021-03-22 15:03:13 +0100

    10. feladat

--- fogyasztas.js
+++ fogyasztas.js
@@ -5,14 +5,17 @@ while(szam.test(km) == false || km > 1000){
 }
 
 let tankolas = prompt("Hány litert tankoltás?")
-while (tankolas > 1 && tankolas < 50){
+while (tankolas > 1 && tankolas > 50){
     tankolas = prompt("Hány litert tankoltás?")
 }
 
+
 let autok = {
-    evjarat = 2000,
-    automarka = "Citroen",
-    fogyaszt = function {
-        console.log(evjarat, "évjáratú", automarka, "fogyasztasa", "liter/100km" )
+    evjarat : 2000,
+    automarka : "Citroen",
+    fogyaszt : function (){
+        console.log(autok.evjarat, "évjáratú", autok.automarka, "fogyasztasa liter/100km" )
     }
 }
+
+autok.fogyaszt()
