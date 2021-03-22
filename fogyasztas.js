5ead9aa346bb884063890462bf766c0fe21b696d
Jenovai <andrea.jenovai@gmail.com>
2021-03-22 13:52:12 +0100

    8. feladat

--- fogyasztas.js
+++ fogyasztas.js
@@ -4,8 +4,12 @@ while(szam.test(km) == false || km < 1000){
     km = prompt("Hány kilométert mentél?")
 }
 
-
 let tankolas = prompt("Hány litert tankoltás?")
 while (tankolas > 1 && tankolas < 50){
     tankolas = prompt("Hány litert tankoltás?")
+}
+
+let autok = {
+    evjarat = 2000,
+    automarka = "Citroen"
 }
